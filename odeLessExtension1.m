fd208fc197861c1de3486e2f5cabbc486f429b1b
Steven4294 <stevenpetteruti@Steven-Petterutis-MacBook-Pro-2.local>
2015-10-15 23:53:06 -0400

    added contour plots

--- odeLessExtension1.m
+++ odeLessExtension1.m
@@ -1,4 +1,6 @@
 %% First Extension
+close all
+clear all
 
 M1 = 150;
 V1 = 30;
@@ -9,17 +11,17 @@ V2 = 100;
 % constants
 K = 200;
 
-D1 = 1000;
+D1 = 100;
 D2 = 380;
-D3 = 1000;
+D3 = 200;
 
-B1 = 500;
-B2 = 500;
-B3 = 500;
+B1 = 800;
+B2 = 530;
+B3 = 20;
 
-A1 = 100;
-A2 = 100;
-A3 = 100;
+A1 = 150;
+A2 = 240;
+A3 = 300;
 
 a = .2;
 alpha = .6;
@@ -29,19 +31,58 @@ tf = 100;
 
 N = 1000;
 t = linspace(0, 100, N+1); % have to add one to make it the same length as M_i's.
+B1_array = linspace(100, 200, 20);
+B2_array = linspace(100, 200, 20);
+B3_array = linspace(100, 200, 20);
+a_array = linspace(0, .5, 40);
+alpha_array = linspace(.5, 1, 40);
+
 dt = t(2) - t(1);
-for i=1:N
-    M1_dot = (a*V1(i)*(1 - (V1(i)/(K - V1(i)) ))) - (D1*M1(i)*((M2(i)^2/(B1+M2(i)^2))*(M1(i)/(A1+M1(i)^3))));
-   
-    V1_dot =  (alpha - a)*V1(i)*(1 - (V1(i)/(K-M1(i)))) -  (V1(i)*D2*((M2(i)^2/(B2+M2(i)^2))*(M1(i)/(A2+M1(i)^2))));
-    
-    M2_dot = - M2(i) * (M2(i)^2/(B3 + M2(i)^3)) * (M1(i)/(A3 + M1(i)^2)) * D3;
-    V2_dot = 0;
-    
-    M1(i+1) = M1(i) + M1_dot * dt;
-    M2(i+1) = M2(i) + M2_dot * dt;
-    V1(i+1) = V1(i) + V1_dot * dt;
-    V2(i+1) = V2(i) + V2_dot * dt;
+
+for j=1:length(B1_array)
+    B1 = B1_array(j);
+    for k = 1:length(B2_array)
+        B2 = B2_array(k);
+        
+        for i=1:N
+            M1_dot = (a*V1(i)*(1 - (V1(i)/(K - V1(i)) ))) - (D1*M1(i)*((M2(i)^2/(B1+M2(i)^2))*(M1(i)/(A1+M1(i)^3))));
+
+            V1_dot =  (alpha - a)*V1(i)*(1 - (V1(i)/(K-M1(i)))) -  (V1(i)*D2*((M2(i)^2/(B2+M2(i)^2))*(M1(i)/(A2+M1(i)^2))));
+
+            M2_dot = - M2(i) * (M2(i)^2/(B3 + M2(i)^3)) * (M1(i)/(A3 + M1(i)^2)) * D3;
+            V2_dot = 0;
+
+            M1(i+1) = M1(i) + M1_dot * dt;
+            M2(i+1) = M2(i) + M2_dot * dt;
+            V1(i+1) = V1(i) + V1_dot * dt;
+            V2(i+1) = V2(i) + V2_dot * dt;
+        end
+        M1_end_Bs(j, k) = M1(end);
+        V1_end_Bs(j, k) = V1(end);
+    end 
+end
+
+for j=1:length(a_array)
+    a = a_array(j);
+    for k = 1:length(alpha_array)
+        alpha = alpha_array(k);
+        
+        for i=1:N
+            M1_dot = (a*V1(i)*(1 - (V1(i)/(K - V1(i)) ))) - (D1*M1(i)*((M2(i)^2/(B1+M2(i)^2))*(M1(i)/(A1+M1(i)^3))));
+
+            V1_dot =  (alpha - a)*V1(i)*(1 - (V1(i)/(K-M1(i)))) -  (V1(i)*D2*((M2(i)^2/(B2+M2(i)^2))*(M1(i)/(A2+M1(i)^2))));
+
+            M2_dot = - M2(i) * (M2(i)^2/(B3 + M2(i)^3)) * (M1(i)/(A3 + M1(i)^2)) * D3;
+            V2_dot = 0;
+
+            M1(i+1) = M1(i) + M1_dot * dt;
+            M2(i+1) = M2(i) + M2_dot * dt;
+            V1(i+1) = V1(i) + V1_dot * dt;
+            V2(i+1) = V2(i) + V2_dot * dt;
+        end
+        M1_end_alpha(j, k) = M1(end);
+        V1_end_alpha(j, k) = V1(end);
+    end 
 end
 
 xmin = 0;
@@ -65,3 +106,44 @@ subplot(2,2,4)
 plot(t, V2, 'o')
 axis([xmin,xmax,ymin,ymax])
 title('V_2')
+
+[X Y] = meshgrid(B1_array, B2_array); %for plotting purposes
+
+figure(2)
+subplot(2,2,1)
+surf(X, Y, M1_end_Bs)
+xlabel('B_1')
+ylabel('B_2')
+zlabel('M_1')
+subplot(2,2,2)
+surf(X, Y, V1_end_Bs)
+xlabel('B_1')
+ylabel('B_2')
+zlabel('V_1')
+
+[X Y] = meshgrid(alpha_array, a_array); %for plotting purposes
+
+
+figure(3)
+subplot(2,2,1)
+surf(X, Y, M1_end_alpha)
+xlabel('alpha')
+ylabel('a')
+zlabel('M_1')
+subplot(2,2,2)
+surf(X, Y, V1_end_alpha)
+xlabel('alpha')
+ylabel('a')
+zlabel('V_1')
+
+subplot(2,2,3)
+contour(X, Y, M1_end_alpha)
+title('contour for M_1')
+xlabel('alpha')
+ylabel('a')
+
+subplot(2,2,4)
+contour(X, Y, V1_end_alpha)
+title('contour for V_1')
+xlabel('alpha')
+ylabel('a')
