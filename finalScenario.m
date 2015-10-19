fff2a91fdb7be3e1fc5a085d101b77cc52c44bdd
Steven4294 <stevenpetteruti@Steven-Petterutis-MacBook-Pro-2.local>
2015-10-18 23:35:32 -0400

    fixed stability for extension2

--- finalScenario.m
+++ finalScenario.m
@@ -3,26 +3,10 @@ close all
 clear all
 % This is when M1 > 0 and V1 and M2 are 0
 [ t,P ] = solveExtension2(100,300, 8.17, 200,  1.0/6.0, 1/6.0, .5, .5, 30, 30, 30, 30, 100);
-subplot(2,2,1);
 plot(t,P);
 xlabel('time');
 ylabel('# individuals');
-title('M1 > 0');
-clear all;
-% This is when M1 > 0 and V1 > 0 and M2 = 0
-[ a,b ] = solveExtension2(100,300, 8.17, 200,  1.0/6.0, 1/6.0, .5, .5, 60, 30, 30, 30, 100);
-subplot(2,2,2);
-plot(a,b);
-xlabel('time');
-ylabel('# individuals');
-title('V1, M1 > 0');
+title('V1 = 0');
+
 legend('M1','V1','M2', 'V2');
 clear all;
-% This is when M2 > 0 and M1 and V1 equal 0
-[ t,P ] = solveExtension2(300,100, 8.17, 200,  1.0/6.0, 1/6.0, .5, .5, 30, 60, 30, 30, 100);
-subplot(2,2,3);
-plot(t,P);
-xlabel('time');
-ylabel('# individuals');
-title('M2 > 0');
-% This is the function called by the above code
